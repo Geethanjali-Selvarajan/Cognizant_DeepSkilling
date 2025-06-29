574f3a9fa94eabd2386e8c8fbc9a28726b54c99d
Geethanjali-Selvarajan <150216904+Geethanjali-Selvarajan@users.noreply.github.com>
2025-06-29 22:46:19 +0530

    Week2_HandsOn

---
+++ week2/Week2_SL4J Logging_HandsOn/Logging Error Messages and Warning Levels/demo/src/test/java/BankService.java
@@ -0,0 +1,21 @@
+import java.util.logging.Logger;
+public class BankService {
+
+    private static final Logger logger = Logger.getLogger(BankService.class.getName());
+
+    public void transferFunds(String fromAccount, String toAccount, double amount) {
+        if (amount <= 0) {
+            logger.warning("Transfer amount must be positive!");
+            return;
+        }
+
+        
+        logger.info("Transferring $" + amount + " from " + fromAccount + " to " + toAccount);
+    }
+
+    public static void main(String[] args) {
+        BankService service = new BankService();
+        service.transferFunds("ACC001", "ACC002", 1000);
+        service.transferFunds("ACC003", "ACC004", -50); 
+    }
+}
